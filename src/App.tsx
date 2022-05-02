06705d7dc2f0ce3bbb04e86de896632967671d68
Bruna Nunes <bruna.f.n@hotmail.com>
2022-05-02 20:57:19 -0300

    Aula 1

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Widget } from './components/Widget'
+
+export function App() {
+
+  return (
+    <>
+    <h1 className='bg-purple-500 text-violet-100 p-2 m-2 rounded hover:bg-violet-800 transition-colors'>Hello World</h1>
+    <Widget />
+    </>
+  )
+}
